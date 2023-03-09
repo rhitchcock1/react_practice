bd135004d015562d753b45224a774380cb12cf05
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-03-09 11:32:24 -0500

    updated contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,15 @@ function Contact(){
     return(
         <div>
             <h1>Contact</h1>
+            <p>make a reservation </p>
+            <form>
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Phone" />
+                <input type="date" placeholder="Date" />
+                <input type="time" placeholder="Time" />
+                <input type="text" placeholder="Any additional details" />
+            </form>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ function Home(){
     return (
         <div>
             <h1>Welcome Home!</h1>
+            <img id="food-pic" src="https://www.food-management.com/sites/food-management.com/files/styles/article_featured_retina/public/gallery_promo_image/Auburn%20High%20School%20Cafeteria%20copy.jpg?itok=sIEERKZz" alt="food hall"/>
         </div>
     )
 
